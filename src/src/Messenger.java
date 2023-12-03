aa246bab175a48329a34f8eb2245d054a979f676
Xels034 <59102313+xels034@users.noreply.github.com>
2023-12-03 12:42:46 +0100

    - Fix warnings for latest java version

--- src/src/Messenger.java
+++ src/src/Messenger.java
@@ -9,22 +9,22 @@ public class Messenger {
   private static HashMap<Message.M_TYPE,LinkedList<Handler>> handleMap = new HashMap<>();
   private static LinkedList<Message> mQueue = new LinkedList<>();
   private static Object monitor = new Object();
-  
+
   public static void subscribe(Handler h, Message.M_TYPE msgT){
     synchronized(monitor){
       if(!handleMap.containsKey(msgT)){
-        handleMap.put(msgT, new LinkedList<Handler>());
+        handleMap.put(msgT, new LinkedList<>());
       }
       handleMap.get(msgT).add(h);
     }
   }
-  
+
   public static void subscribe(Handler h, Collection<Message.M_TYPE> msgT){
     for(Message.M_TYPE i : msgT){
       subscribe(h, i);
     }
   }
-  
+
   public static void unsubscribe(Handler h, Message.M_TYPE msgT){
     synchronized(monitor){
       if(handleMap.containsKey(msgT)){
@@ -32,7 +32,7 @@ public class Messenger {
       }
     }
   }
-  
+
   public static void unsubscribe(Handler h){
     synchronized(monitor){
       for(Message.M_TYPE msgT : handleMap.keySet()){
@@ -40,7 +40,7 @@ public class Messenger {
       }
     }
   }
-  
+
   public static void fire(Message m){
     if(handleMap.containsKey(m.getMsgType())){
       for(Handler h : handleMap.get(m.getMsgType())){
@@ -50,12 +50,12 @@ public class Messenger {
       }
     }
   }
-  
+
   public static void send(Message m){
     //System.out.println("[MESSENGER] Msg of type "+m.getMsgType()+" added to Queue");
     mQueue.add(m);
   }
-  
+
   @SuppressWarnings("unchecked")
   public static void update(){
     LinkedList<Message> workList = (LinkedList<Message>)mQueue.clone();
